d1259ff385feb8c53449de32a803c3d98a5bf62d
gitHub <cyc00201@gmail.com>
2020-05-14 13:25:38 +0800

    Initial Commit

---
+++ noveler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  noveler
+//
+//  Created by User06 on 2020/5/14.
+//  Copyright © 2020 bc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
